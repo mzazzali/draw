db70ec8ac6c8461327ac1f4d185d6945e5c6f0b8
uzazzmi <michael.zazzali@pearson.com>
2014-01-07 09:30:08 -0500

    cleaning up repository

--- .repl/zee3/draw/defaults.js
+++
@@ -1,7 +0,0 @@
-goog.provide('zee3.draw.defaults');
-goog.require('cljs.core');
-zee3.draw.defaults.HIT_TEST_STAGE = "color-map-stage";
-zee3.draw.defaults.base = cljs.core.ObjMap.fromObject(["\uFDD0'x","\uFDD0'y","\uFDD0'width","\uFDD0'height","\uFDD0'lineWidth","\uFDD0'fillStyle","\uFDD0'Strokestyle"],{"\uFDD0'x":0,"\uFDD0'y":0,"\uFDD0'width":100,"\uFDD0'height":100,"\uFDD0'lineWidth":1,"\uFDD0'fillStyle":"#FFFFFF","\uFDD0'Strokestyle":"#FFFFFF"});
-zee3.draw.defaults.rounded_rectangle = cljs.core.merge.call(null,zee3.draw.defaults.base,cljs.core.ObjMap.fromObject(["\uFDD0'cornerRadius"],{"\uFDD0'cornerRadius":5}));
-zee3.draw.defaults.circle = cljs.core.merge.call(null,zee3.draw.defaults.base,cljs.core.ObjMap.fromObject(["\uFDD0'centerX","\uFDD0'centerY","\uFDD0'radius","\uFDD0'lineWidth"],{"\uFDD0'centerX":0,"\uFDD0'centerY":100,"\uFDD0'radius":100,"\uFDD0'lineWidth":1}));
-zee3.draw.defaults.text = cljs.core.ObjMap.fromObject(["\uFDD0'fillStyle","\uFDD0'x","\uFDD0'y","\uFDD0'font","\uFDD0'text"],{"\uFDD0'fillStyle":"#FFFFFF","\uFDD0'x":0,"\uFDD0'y":0,"\uFDD0'font":"bold 16px Arial","\uFDD0'text":""});
